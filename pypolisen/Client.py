05a5df8187d96828e8ce4245a5356c9b52bedc31
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2018-07-17 16:20:51 +0200

    cleanup

--- pypolisen/Client.py
+++ pypolisen/Client.py
@@ -2,6 +2,11 @@ import json
 from requests import Session
 from bs4 import BeautifulSoup
 from pypolisen.utils import tryreturn, set_attr
+from pypolisen.constants import (
+    ITEM_PAGE_TEXT_CSS_QUERY,
+    GET_ITEMS_QUERY_DEFAULTS,
+    EMPTY_LIST
+)
 
 
 class Client(object):
@@ -21,42 +26,42 @@ class Client(object):
                 ).text
             ),
             ValueError,
-            []
+            EMPTY_LIST
         )
 
     def get_items(self, location_id):
         return tryreturn(
             map(
-                lambda x: set_attr(x, 'text', self.scrape_element_text(
-                    self.base_url + x['Url'], '.editorial-html'
-                )),
+                lambda x: set_attr(x, 'text', self.get_item_text(x)),
                 json.loads(
                     self.session.post(
                         self.items_route,
-                        data={
-                            'SelectedLocationId': location_id,
-                            'ContentId': 7069,
-                            'PageIndex': 1,
-                            'IncludeChildIndexes': False,
-                            'FilterOnSelectedLocation': True,
-                            'filter': None,
-                            'GeoAreaType': 1,
-                            'Latitude': 0,
-                            'Longitude': 0,
-                            'ListItemType': 1,
-                            'PropertyIds': []
-                        }
+                        data=dict(
+                            SelectedLocationId=location_id,
+                            **GET_ITEMS_QUERY_DEFAULTS
+                        )
                     ).text
                 )['List']
             ),
             ValueError,
-            []
+            EMPTY_LIST
         )
     
-    def scrape_element_text(self, url, selector):
+    def get_item_document(self, item):
+        return BeautifulSoup(
+            self.session.get(self.base_url + item['Url']).text,
+            'html.parser'
+        )
+
+    def get_item_text(self, item):
+        return self.scrape_element_text(
+            self.get_item_document(item),
+            ITEM_PAGE_TEXT_CSS_QUERY
+        )
+
+    def scrape_element_text(self, document, selector):
         return tryreturn(
-            BeautifulSoup(self.session.get(url).text, 'html.parser')
-            .select(selector)[0].text,
+            document.select(selector)[0].text,
             IndexError,
             None
         )
---
+++ pypolisen/constants.py
@@ -0,0 +1,14 @@
+ITEM_PAGE_TEXT_CSS_QUERY = '.editorial-html'
+GET_ITEMS_QUERY_DEFAULTS = {
+    'ContentId': 7069,
+    'PageIndex': 1,
+    'IncludeChildIndexes': False,
+    'FilterOnSelectedLocation': True,
+    'filter': None,
+    'GeoAreaType': 1,
+    'Latitude': 0,
+    'Longitude': 0,
+    'ListItemType': 1,
+    'PropertyIds': []
+}
+EMPTY_LIST = []
